77b73b8a4d5b229ec1040df0bb299f745c0a9f25
Stefano Carmignola <stefano.crmg@gmail.com>
2022-09-12 18:52:23 +0200

    removes useless routeerrors and adds tests

--- src/hyper-ts-utilities/defaultErrorHandler.ts
+++ src/hyper-ts-utilities/defaultErrorHandler.ts
@@ -1,3 +1,4 @@
+import { ServerEnv } from 'src/serverEnv'
 import * as C from 'fp-ts/Console'
 import { RouteError } from './routeError'
 import { flow, pipe } from 'fp-ts/function'
@@ -10,23 +11,28 @@ import {
   sendUnauthorized,
 } from './responses'
 
-export const defaultErrorHandler: <Env>(
+export const defaultErrorHandler: (
   e: RouteError,
-) => RM.ReaderMiddleware<Env, H.StatusOpen, H.ResponseEnded, never, void> =
-  RouteError.match({
-    NotFound: () => RM.fromMiddleware(sendNotFound),
-    SomeException: ({ exception }) =>
-      pipe(
-        C.error(`Some exception: ${exception}`),
-        RM.rightIO,
-        RM.ichainMiddlewareK(() => sendInternalServerError),
-      ),
-    DecodingFailure: ({ errors }) =>
-      pipe(
-        C.error(`Decoding failure: ${errors}`),
-        RM.rightIO,
-        RM.ichainMiddlewareK(() => sendInternalServerError),
-      ),
-    BadRequest: flow(sendBadRequest, RM.fromMiddleware),
-    Unauthorized: ({ message }) => RM.fromMiddleware(sendUnauthorized(message)),
-  })
+) => RM.ReaderMiddleware<
+  ServerEnv,
+  H.StatusOpen,
+  H.ResponseEnded,
+  never,
+  void
+> = RouteError.match({
+  NotFound: () => RM.fromMiddleware(sendNotFound),
+  SomeException: ({ exception }) =>
+    pipe(
+      C.error(`Some exception: ${exception}`),
+      RM.rightIO,
+      RM.ichainMiddlewareK(() => sendInternalServerError),
+    ),
+  DecodingFailure: ({ errors }) =>
+    pipe(
+      C.error(`Decoding failure: ${errors}`),
+      RM.rightIO,
+      RM.ichainMiddlewareK(() => sendInternalServerError),
+    ),
+  BadRequest: flow(sendBadRequest, RM.fromMiddleware),
+  Unauthorized: ({ message }) => RM.fromMiddleware(sendUnauthorized(message)),
+})

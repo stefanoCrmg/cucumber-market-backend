2be475df22b9f57d26e9bb82e04205550c39df23
Stefano Carmignola <stefano.crmg@gmail.com>
2022-09-17 19:25:22 +0200

    observe startup errors

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,16 @@ import * as C from 'fp-ts/Console'
 import * as TE from 'fp-ts/TaskEither'
 import { pipe } from 'fp-ts/lib/function'
 import { exit } from 'process'
-import { makeServerEnv, makeServer } from './makeServer'
+import {
+  makeServerEnv,
+  makeServer,
+  observeStartupServerErrors,
+} from './makeServer'
 
 const runServer = pipe(
   makeServerEnv,
   TE.map(makeServer),
-  TE.orElseFirstIOK((e) => C.error(`Error: ${JSON.stringify(e)}`)),
+  TE.orElseFirstIOK(observeStartupServerErrors),
   TE.match(
     () => exit(1),
     (_) => _.listen(3000, C.log('Express listening on port 3000')),
